bf7a196ef1e28b6430f552dc582bd22317386833
LiamBull <lphilipbull@gmail.com>
2020-05-09 14:20:06 -0400

    Add array cast

--- src/SQLGen/DiffToSQL/AddTableSQL.php
+++ src/SQLGen/DiffToSQL/AddTableSQL.php
@@ -3,20 +3,24 @@
 use DBDiff\SQLGen\SQLGenInterface;
 
 
-class AddTableSQL implements SQLGenInterface {
+class AddTableSQL implements SQLGenInterface
+{
 
-    function __construct($obj) {
+    function __construct($obj)
+    {
         $this->obj = $obj;
     }
-    
-    public function getUp() {
+
+    public function getUp()
+    {
         $table = $this->obj->table;
         $connection = $this->obj->connection;
         $res = $connection->select("SHOW CREATE TABLE `$table`");
-        return $res[0]['Create Table'].';';
+        return ((array) $res[0])['Create Table'].';';
     }
 
-    public function getDown() {
+    public function getDown()
+    {
         $table = $this->obj->table;
         return "DROP TABLE `$table`;";
     }
